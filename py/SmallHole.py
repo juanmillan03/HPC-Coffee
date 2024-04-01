b30fe4a6175209bbc32cc782a164367ccd55090e
ElJoho <johalopezari@unal.edu.co>
2024-04-01 17:18:27 -0500

    Update SmallHole.py
    Se añadieron eitquetas en la grafica de smallHole que indican que es cada curva

--- py/SmallHole.py
+++ py/SmallHole.py
@@ -5,14 +5,15 @@ import numpy as np
 from scipy.stats import norm
 
 def dispercion(data_path):
-    datos=pd.read_csv(data_path,sep="\t" ,names=['t','te',"muestra"])
+    datos = pd.read_csv(data_path, sep="\t", names=['t', 'te', "muestra"])
     
-    fig1, ax1 = plt.subplots( )
-    ax1.scatter(datos['t'],datos['te'],s=2)
-    ax1.scatter(datos['t'],datos['muestra'],s=2)
+    fig1, ax1 = plt.subplots()
+    ax1.scatter(datos['t'], datos['te'], s=2, label="Theorical cream molecules", color='blue')  # Añadido label y color
+    ax1.scatter(datos['t'], datos['muestra'], s=2, label="Simulated cream molecules", color='orange')  # Añadido label y color
     ax1.set_xlabel("time(10⁶unit)")
-    ax1.set_ylabel("Entropy")
+    ax1.set_ylabel("Number of cream molecules inside the container")
     ax1.grid(True, linewidth=0.1)
+    ax1.legend()  # Muestra las leyendas en la gráfica
     plt.savefig("./pdf/Smallhole.pdf")
     
     
